3bf8d97041530d9138f4d75f6efda18d309701a2
yui1337 <egolunov095@gmail.com>
2025-03-31 00:00:23 +0400

    updated main

--- lab_1/main.py
+++ lab_1/main.py
@@ -3,19 +3,25 @@ from read_write import *
 from Task_1.encryption import *
 
 
-def main():
-    task1_settings = read_json("settings.json").get("task_1") # Task 1
+def task1() -> None:
+    task1_settings = read_json("settings.json").get("task_1")  # Task 1
     task1_text = get_text(task1_settings["plain_text"])
     task1_key = read_json(task1_settings["key"])
     encrypted_text = encrypt_text(task1_text, task1_key)
     save_text(task1_settings["encrypted_text"], encrypted_text)
 
+
+def task2() -> None:
     task2_settings = read_json("settings.json").get("task_2") # Task 2
     task2_coded_text = get_text(task2_settings["encrypted_text"])
     save_json(task2_settings["frequencies"], freq_analysis(task2_coded_text))
     save_json(task2_settings["automated_key"], create_key(task2_coded_text, read_json(task2_settings["ru_frequencies"])))
     decrypted_text = decrypt(task2_coded_text, read_json(task2_settings["real_key"]))
     save_text(task2_settings["decrypted_text"], decrypted_text)
+
+def main():
+    task1()
+    task2()
 
 
 if __name__ == "__main__":
