958cd129ad2525de56ac92a5c69a729e3ac0e0bc
yui1337 <egolunov095@gmail.com>
2025-03-30 17:02:11 +0400

    Added docstrings, removed unused imports and variables

--- lab_1/Task_2/decryption.py
+++ lab_1/Task_2/decryption.py
@@ -1,24 +1,38 @@
 from collections import Counter
 
-from ..read_write import *
-
 
 def freq_analysis(text: str) -> dict[str, float]:
+    """
+    Creates relative frequency table for given text
+    :param text: The text, which to create frequency table
+    :return: Frequency table in form of a dict
+    """
     cnt = Counter(text)
-    length = len(text)
     freqs = {letter: count / len(text) for letter, count in cnt.items()}
     return dict(sorted(freqs.items(), key=lambda item: item[1], reverse=True))
 
 
-def create_key(encrypted_text: str, ru_freqs: dict) -> dict:
+def create_key(encrypted_text: str, ru_freqs: dict[str, float]) -> dict[str, str]:
+    """
+    Creates decryption key, based on a frequency analysis method
+    :param encrypted_text: Text to decrypt
+    :param ru_freqs: Frequency table for russian language
+    :return: Decryption key in form of a dict
+    """
     cur_text_freq = freq_analysis(encrypted_text)
     key = dict()
     for (letter_text, _), (letter_key, _) in zip(cur_text_freq.items(), ru_freqs.items()):
         key[letter_text] = letter_key
-    save_json("key.json", key)
     return key
 
-def decrypt(encrypted_text: str, key: dict) -> str:
+
+def decrypt(encrypted_text: str, key: dict[str, float]) -> str:
+    """
+    Decrypts text with given key
+    :param encrypted_text: Text to decrypt
+    :param key: key in form of a dict
+    :return: Decrypted text
+    """
     decrypted_text = ""
     for letter in encrypted_text:
         if letter in key:
@@ -26,6 +40,3 @@ def decrypt(encrypted_text: str, key: dict) -> str:
         else:
             decrypted_text += letter
     return decrypted_text
-
-
-
