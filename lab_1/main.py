139c0349182f63bc6c68527d6b20b2bec1e8c7dc
yui1337 <egolunov095@gmail.com>
2025-03-30 14:19:05 +0400

    Created main func for task 2

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,3 +1,4 @@
+from lab_1.Task_2.decryption import *
 from read_write import *
 from Task_1.encryption import *
 
@@ -10,6 +11,11 @@ def main():
     save_text(task1_settings["encrypted_text"], encrypted_text)
 
     #Task 2 Work in progress
+    task2_settings = read_json("settings.json").get("task_2")
+    task2_coded_text = get_text(task2_settings["encrypted_text"])
+    freq_analysis(task2_coded_text)
+    decrypted_text = decrypt(task2_coded_text, read_json(task2_settings["key"]))
+    save_text(task2_settings["decrypted_text"], decrypted_text)
 
 
 if __name__ == "__main__":
