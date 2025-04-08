a2f8d41141be10ae8750be4b94b0983ebe094cb5
yui1337 <egolunov095@gmail.com>
2025-04-08 17:11:45 +0400

    Added C++ random seq generation

---
+++ lab_2/Task_1/isb_sequence_gen.cpp
@@ -0,0 +1,32 @@
+﻿#include <fstream>
+#include <iostream>
+#include <random>
+#include <string>
+
+
+std::string generate_128_bit_sequence() {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<size_t> number(0, 1);
+
+	std::string result;
+	for (size_t i = 0; i < 128; ++i) {
+		result += std::to_string(number(gen));
+	}
+	return result;
+}
+
+void save_string(std::string dir, std::string string_to_save) {
+	std::ofstream out;
+	out.open(dir);
+	out << string_to_save << std::endl;
+	out.close();
+}
+
+int main()
+{
+	std::string seq = generate_128_bit_sequence();
+	save_string("cpp_sequence.txt", seq);
+	std::cout << seq;
+}
+
