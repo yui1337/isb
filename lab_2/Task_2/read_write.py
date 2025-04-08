62c6ba778ae44e33192d875abdd0e10620c27604
yui1337 <egolunov095@gmail.com>
2025-04-08 19:43:43 +0400

    Added functions to work with JSON files

--- lab_2/Task_2/read_write.py
+++ lab_2/Task_2/read_write.py
@@ -1,3 +1,6 @@
+import json
+
+
 def get_text(dir: str) -> str:
     """
     Opens text file from given directory
@@ -9,3 +12,36 @@ def get_text(dir: str) -> str:
             return file.read()
     except FileNotFoundError as e:
         print(f"File does not exist or you entered wrong path: {e}")
+
+def read_json(dir: str) -> dict:
+    """
+    Reads JSON file from given directory
+    :param dir: Path to the JSON file
+    :return: JSON file as a dictionary
+    """
+    try:
+        with open(dir, mode='r', encoding="utf-8") as file:
+            return json.load(file)
+    except FileNotFoundError as e:
+        print(f"File does not exist or you entered wrong path: {e}")
+    except PermissionError as e:
+        print(f"Can't access this file: {e}")
+    except Exception as e:
+        print(f"Something weird happened:{e}")
+
+def save_json(dir: str, data: dict) -> None:
+    """
+    Saves dictionary as json file
+    :param dir: Path to save .json file
+    :param data: Dictionary to save
+    :return: None
+    """
+    try:
+        with open(dir, mode='w', encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False)
+    except FileNotFoundError as e:
+        print(f"File does not exist or you entered wrong path: {e}")
+    except PermissionError as e:
+        print(f"Can't access this file: {e}")
+    except Exception as e:
+        print(f"Something went wrong: {e}")
