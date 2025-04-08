7a867ce088a9834ff063b767777339dc9652cf8a
yui1337 <egolunov095@gmail.com>
2025-04-08 17:23:55 +0400

    Added Java random seq generation

---
+++ lab_2/Task_1/isb_sequence_gen.java
@@ -0,0 +1,15 @@
+import java.security.SecureRandom;
+
+public class Main
+{
+	public static void main(String[] args) {
+		SecureRandom random = new SecureRandom();
+        StringBuilder binaryString = new StringBuilder();
+
+        for (int i = 0; i < 128; i++) {
+            boolean bit = random.nextBoolean();
+            binaryString.append(bit ? '1' : '0');
+        }
+        System.out.println(binaryString);
+	}
+}
