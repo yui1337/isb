77eafd454abe6ed2adcf8f4e6484cb1bdab3e579
yui1337 <egolunov095@gmail.com>
2025-04-14 14:16:55 +0400

    Weird things happened

--- lab_2/Task_1/isb_sequence_gen.java
+++ lab_2/Task_1/isb_sequence_gen.java
@@ -2,15 +2,15 @@ import java.security.SecureRandom;
 
 
 public class Main {
-	public static void main(String[] args) {
-		SecureRandom random = new SecureRandom();
+    public static void main(String[] args) {
+        SecureRandom random = new SecureRandom();
         StringBuilder binaryString = new StringBuilder();
 
         for (int i = 0; i < 128; i++) {
             boolean bit = random.nextBoolean();
-            binaryString.append(bit ? '1' : '0');
+        binaryString.append(bit ? '1' : '0');
         }
         System.out.println(binaryString);
-	}
+    }
 }
 
