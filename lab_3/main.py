68dd7da905d0c080e822277b0907f113843a4c9c
yui1337 <egolunov095@gmail.com>
2025-05-12 15:33:46 +0400

    Updated test main()

--- lab_3/main.py
+++ lab_3/main.py
@@ -1,3 +1,4 @@
+from lab_3.crypto.asymmetric import Asymmetric
 from lab_3.crypto.symmetric import Symmetric
 
 symmetric = Symmetric()
@@ -6,3 +7,11 @@ symmetric.serialization_symmetric_key("crypto/keys/symmetric_key.txt")
 symmetric.encrypt_text("text_files/plain_text.txt", "text_files/cipher_text.txt")
 symmetric.decrypt_text("crypto/keys/symmetric_key.txt", "text_files/cipher_text.txt",
                        "text_files/decipher_text.txt")
+
+asymmetric = Asymmetric()
+asymmetric.generate_asymmetric_keys()
+asymmetric.serialization_public_key("crypto/keys/public_key.txt")
+asymmetric.serialization_private_key("crypto/keys/private_key.txt")
+asymmetric.encrypt_symmetric_key("crypto/keys/public_key.txt", "crypto/keys/symmetric_key.txt",
+                                 "crypto/keys/encrypted_symmetric_key.txt")
+
